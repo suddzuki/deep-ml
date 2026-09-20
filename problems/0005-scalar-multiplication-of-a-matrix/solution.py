77d2760eab0b745312ad05d347b5d2eddb6dd3a7
suddzuki <164516725+suddzuki@users.noreply.github.com>
2026-09-20 16:53:21 +0000

    Update: Scalar Multiplication of a Matrix

--- problems/0005-scalar-multiplication-of-a-matrix/solution.py
+++ problems/0005-scalar-multiplication-of-a-matrix/solution.py
@@ -1,6 +1,7 @@
-import numpy as np
 
 def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
-	a = np.array(matrix)
+	
 
-	return (a * scalar).tolist()
+	a_scalar = [list(map(lambda x: x * scalar, row)) for row in matrix]
+
+	return a_scalar
