91766f1c7a98b7e02362901beb74bfb754fcf236
suddzuki <164516725+suddzuki@users.noreply.github.com>
2026-09-20 17:03:24 +0000

    Update: Calculate 2x2 Matrix Inverse

--- problems/0008-calculate-2x2-matrix-inverse/solution.py
+++ problems/0008-calculate-2x2-matrix-inverse/solution.py
@@ -1,11 +1,16 @@
-import numpy as np
-
 def inverse_2x2(matrix: list[list[float]]) -> list[list[float]] | None:
-    a = np.array(matrix)
+    a, b = matrix[0][0], matrix[0][1]
+    c, d = matrix[1][0], matrix[1][1]
 
-    det = np.linalg.det(a)
+    det = a * d - b * c
 
     if det == 0:
         return None
+    matrix_adj = [
+        [d, -b],
+        [-c, a]
+    ]
 
-    return np.linalg.inv(a).tolist()
+    matrix_inv = [[(1 / det) * val for val in row] for row in matrix_adj]
+
+    return matrix_inv
