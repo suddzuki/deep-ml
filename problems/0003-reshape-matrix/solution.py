462e3f402e77c385c7042ef0d32751c59c1ffef7
suddzuki <164516725+suddzuki@users.noreply.github.com>
2026-09-20 16:32:01 +0000

    Update: Reshape Matrix

--- problems/0003-reshape-matrix/solution.py
+++ problems/0003-reshape-matrix/solution.py
@@ -1,9 +1,19 @@
-import numpy as np
 
 def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
-	an = np.array(a)
-	old_shape = an.shape
+
+	old_shape = (len(a), len(a[0]))
 	reshaped_matrix = []
 	if (new_shape[0] * new_shape[1] == old_shape[0] * old_shape[1]):
-		reshaped_matrix = an.reshape(new_shape).tolist()
+		
+		n = new_shape[0] * new_shape[1] 
+		elems = []
+		for i in range (old_shape[0]):
+			for j in range(old_shape[1]):
+				elems.append(a[i][j])
+		
+
+		reshaped_matrix = [elems[i:i + new_shape[1]] for i in range(0, n, new_shape[1])]
+
+
+
 	return reshaped_matrix
